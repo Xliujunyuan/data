ef9f072523cec6a98f7b226c14a901351f50bd50
ljy <1571102750@qq.com>
2018-02-17 15:16:44 +0800

    Revert "Revert "防止后期出错""
    This reverts commit b4fd3c08f09662fa5ca823099966b2f82d561d85.

---
+++ bookShop2/js/jquery.js
@@ -0,0 +1,108 @@
+﻿
+
+$(function(){/* 下拉菜单 */
+ 		  $("#right").find("li>div").hide().parent().hover(
+ 		  function(){
+ 		  /*this是li*/
+ 		  $(this).addClass("rightulli")
+ 		  .children("div").addClass("navdiv").show()
+ 		  /*a*/
+ 		  .children().children().addClass("navdiva")
+ 		  /*nav*/
+ 		  .parent().parent()
+ 		  /*div*/
+ 		  .children().hover(function(){ 
+ 		  	
+ 		  	/*这个是li下的div元素的div*/
+			
+ 				if($(this).prop("id")=='')
+ 				{		
+ 		  			$(this).css("background-color","#F5F5F5");
+ 		  		}
+ 		  },
+ 		  function(){
+ 		  if($(this).prop("id")==''){
+ 		  $(this).css("background-color","");
+ 		  }
+ 		  
+ 		  })
+ 		  }
+ 		  
+ 		  ,function(){$(this).removeClass("rightulli").children("div").hide()}
+ 		   
+ 		  );
+ 		  				
+ 		 $("#right").find("li").hover(function(){
+ 		 				$(this).children("a").css("color","red") 		
+ 		 },
+ 		 function(){
+ 		 				$(this).children("a").css("color","") 
+ 		 } 
+ 		 )})
+ 		  
+ 		
+ 	  		
+ 	  				$(function(){
+ 	  					
+ 	  					$(".f").children(".ntn").hide().parent().hover(
+ 	  							
+ 	  							function(){
+ 	  										
+ 	  								$(this).children("a").addClass("menunav").css("color","red").next().show();
+ 	  											
+ 	  							}
+ 	  							,
+ 	  							
+ 	  							function(){
+ 	  								
+ 	  								$(this).children("a").removeClass("menunav").css("color","");
+ 	  								$(this).children("div").hide();
+ 	  							}
+ 	  					)
+ 	  				})
+ 	  			  			
+ 	  				
+ 	  				$(function(){
+ 	  							
+ 	  							$(".one").each(
+ 	  									function(){
+ 	  										if($(this).children(".clear").prevAll("div").length==2)
+ 	  											{
+ 	  											
+ 	  											$(this).children(":nth-child(2)").addClass("oneson");
+ 	  											
+ 	  											}
+ 	  										
+ 	  									}
+ 	  							)
+ 	 	  					
+ 	  				})
+ 	  				
+ 	  				
+ 	  				
+ 	  				
+ 	  			/* 判断浏览器 */
+ 	  			/* 火狐 */
+ 	  			$(function(){
+ 	  				if($.browser.mozilla)   
+ 	  			{		
+ 	  					if($.browser.version>20)
+ 	  						{
+ 	  				 $(".one").css("margin","-5px 10px 0px 0px").children("a").css({"margin":"0px 10px 0px 0px","font-size": "12px",
+ 	  				"line-height": "13px"})
+ 	  						}
+ 	  					else{
+ 	  						$(".one").css("margin","-3px 10px 0px 0px").children("a").css({"margin":"0px 10px 0px 0px","font-size": "12px",
+ 	  		 	  				"line-height": "13px"})
+ 	  					}
+ 	  			}
+ 	  				
+ 	  				
+ 	  			
+ 	   
+ 	  			})			 
+ 	  			
+ 	  			
+ 	  		
+ 	  			
+ 	  			
---
+++ bookShop2/成功的php登录/connectdb.php
@@ -0,0 +1,90 @@
+﻿<?php
+
+
+header("Content-type:text/html;charset=utf-8");
+
+class Mysql{  
+         private static $host="localhost";  
+         private static $user="root";  
+         private static $password="abc";  
+         private static $dbName="registerdb";                     //数据库名  
+         private static $charset="utf8";                    //字符编码  
+         private static $port="3306";                       //端口号  
+         private  $conn=null;  
+
+         function __construct(){  
+             $this->conn=new mysqli(self::$host,self::$user,self::$password,self::$dbName,self::$port);  
+             if(!$this->conn)  
+             {  
+                   die("数据库连接失败！".$this->conn->connect_error);  
+             }else{  
+                 echo "连接成功！";  
+                 $this->conn->query("set names ".self::$charset);  
+             }  
+             
+         }  
+           
+             
+    
+           
+           
+         //执行sql语句  
+         function sql($sql){  
+             $res=$this->conn->query($sql);  
+         if(!$res)  
+              {  
+                   echo "数据操作失败";  
+              }  
+              else  
+              {  
+                   if($this->conn->affected_rows>0)  
+                   {  
+                         return $res;  
+                   }  
+                   else  
+                   {  
+                        echo "0行数据受影响！";  
+                   }  
+              }  
+                
+         }  
+           
+         //返回受影响数据行数  
+         function getResultNum($sql){  
+               $res=$this->conn->query($sql);  
+              $line=mysqli_num_rows($res); 
+               if($line>0)
+               {
+               echo "<table border='1'>";
+               echo "<tr>";
+               echo "<td>姓名</td>";
+                echo "<td>邮箱</td>";
+                 echo "<td>性别</td>";
+               echo "</tr>";
+               
+             
+               while($row=mysqli_fetch_array($res))
+               {      echo "<tr>";
+                        echo "<td>".$row['name']."</td>";
+                        echo "<td>".$row['email']."</td>";
+                        echo "<td>".$row['gender']."</td>";
+                        echo "</tr>";
+                    
+               }
+               
+               echo "</table>";
+               
+               }
+           }  
+           
+         //关闭数据库  
+         public function close()  
+         {  
+             @mysqli_close($this->conn);  
+         }  
+ 
+
+
+
+}
+?>
